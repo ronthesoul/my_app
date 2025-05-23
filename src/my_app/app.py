1bf9c24ae2926dfb4afc689cdbe3b176da3be741
Ron Negrov <m4gapower@gmail.com>
2025-05-23 14:03:52 +0300

    added service

--- src/my_app/app.py
+++ src/my_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, render_template_string
 
 app = Flask(__name__)
 
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/os')
+def printos():
+    user_agent = request.headers.get('User-Agent')
+    return render_template_string(f"<h1>Welcome, your connection is from a {user_agent} based system.</h1>")
+
 if __name__ == '__main__':
+<<<<<<< HEAD
     app.run(host="0.0.0.0", port=8000, debug=True)
+=======
+    app.run(debug=True)
+>>>>>>> 04d729a (added service)
